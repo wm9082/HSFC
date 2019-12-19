33721a7a35a70679e707797fda266425970fe79d
Will Mason <wm9082@hereford.ac.uk>
2019-12-19 15:31:05 +0000

    commit

---
+++ QueueProject/QueueProject/QueueCircular.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProject
+{
+    class QueueCircular
+    {
+        private string[] queue;
+        private int front, back;
+        public void Add(string obj)
+        {
+            if (back < queue.Length)
+            {
+                queue[back] = obj;
+                back = (back+1) % queue.Length;
+            }
+        }
+        public QueueCircular(int length)
+        {
+            queue = new string[length];
+        }
+        public string Remove()
+        {
+            if (front != back)
+            {
+                front = (front+1) % queue.Length;
+                return queue[front - 1];
+            }
+            return null;
+        }
+        public int GetLength()
+        {
+            return back;
+        }
+        public override string ToString()
+        {
+            string List = "";
+            for (int i = front; i < back; i++)
+            {
+                List = List + queue[i] + ",";
+            }
+            return List;
+        }
+    }
+}
