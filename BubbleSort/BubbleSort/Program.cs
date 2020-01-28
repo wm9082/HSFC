9188fde2f0f6bc4a52b71c5ba9a3d927753dafc3
Will Mason <wm9082@hereford.ac.uk>
2020-01-28 14:52:42 +0000

    commit

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[100]
+            Console.WriteLine("Please enter your list of names to be reversed in the order which you would like them to be reversed:")
+            while(Console.ReadLine() != "STOP") 
+            {
+
+
+            }
+        }
+    }
+}
