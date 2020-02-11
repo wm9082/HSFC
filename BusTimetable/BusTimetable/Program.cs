fb1e37a77085b8be878de72b87b5c82327515849
Will Mason <wm9082@hereford.ac.uk>
2020-02-11 14:51:44 +0000

    commit

---
+++ BusTimetable/BusTimetable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BusTimetable.Properties;
+
+namespace BusTimetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            string fileContent = Resources.Data;
+            StringReader reader = new StringReader(fileContent);
+            String[] theDictionary = new String[100];
+            int nextFreeLocation = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                theDictionary[nextFreeLocation] = line;
+                nextFreeLocation++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
